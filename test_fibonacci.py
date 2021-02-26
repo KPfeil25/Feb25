4f848c351b84a37cfbd5f91d427e96960c1aa4ce
Kevin Pfeil <47901570+KPfeil25@users.noreply.github.com>
2021-02-25 22:03:00 -0800

    Delete test_fibonacci.py

--- test_fibonacci.py
+++
@@ -1,9 +0,0 @@
-import fibonacci
-import unittest
-
-class fibTesting(unittest.TestCase):
-    def test1(self):
-        self.assertEqual(fibonacci.fib(4), 3)
-    
-if __name__ == "__main__":
-    unittest.main()
